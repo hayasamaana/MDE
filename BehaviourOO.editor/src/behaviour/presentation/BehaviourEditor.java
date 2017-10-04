8924189af9a57033761861a40806e7c004d4a666
hayasamaana <haya.sammaneh@gmail.com>
2017-10-04 17:35:33 +0300

    Final

--- BehaviourOO.editor/src/behaviour/presentation/BehaviourEditor.java
+++ BehaviourOO.editor/src/behaviour/presentation/BehaviourEditor.java
@@ -404,8 +404,6 @@ public class BehaviourEditor
 	 */
 	protected EContentAdapter problemIndicationAdapter =
 		new EContentAdapter() {
-			protected boolean dispatching;
-
 			@Override
 			public void notifyChanged(Notification notification) {
 				if (notification.getNotifier() instanceof Resource) {
@@ -421,26 +419,21 @@ public class BehaviourEditor
 							else {
 								resourceToDiagnosticMap.remove(resource);
 							}
-							dispatchUpdateProblemIndication();
+
+							if (updateProblemIndication) {
+								getSite().getShell().getDisplay().asyncExec
+									(new Runnable() {
+										 public void run() {
+											 updateProblemIndication();
+										 }
+									 });
+							}
 							break;
 						}
 					}
 				}
 				else {
 					super.notifyChanged(notification);
-				}
-			}
-
-			protected void dispatchUpdateProblemIndication() {
-				if (updateProblemIndication && !dispatching) {
-					dispatching = true;
-					getSite().getShell().getDisplay().asyncExec
-						(new Runnable() {
-							 public void run() {
-								 dispatching = false;
-								 updateProblemIndication();
-							 }
-						 });
 				}
 			}
 
@@ -453,7 +446,14 @@ public class BehaviourEditor
 			protected void unsetTarget(Resource target) {
 				basicUnsetTarget(target);
 				resourceToDiagnosticMap.remove(target);
-				dispatchUpdateProblemIndication();
+				if (updateProblemIndication) {
+					getSite().getShell().getDisplay().asyncExec
+						(new Runnable() {
+							 public void run() {
+								 updateProblemIndication();
+							 }
+						 });
+				}
 			}
 		};
 
@@ -651,11 +651,14 @@ public class BehaviourEditor
 			}
 
 			if (markerHelper.hasMarkers(editingDomain.getResourceSet())) {
-				try {
-					markerHelper.updateMarkers(diagnostic);
-				}
-				catch (CoreException exception) {
-					BehaviourEditorPlugin.INSTANCE.log(exception);
+				markerHelper.deleteMarkers(editingDomain.getResourceSet());
+				if (diagnostic.getSeverity() != Diagnostic.OK) {
+					try {
+						markerHelper.createMarkers(diagnostic);
+					}
+					catch (CoreException exception) {
+						BehaviourEditorPlugin.INSTANCE.log(exception);
+					}
 				}
 			}
 		}
@@ -1036,7 +1039,6 @@ public class BehaviourEditor
 
 				selectionViewer = (TreeViewer)viewerPane.getViewer();
 				selectionViewer.setContentProvider(new AdapterFactoryContentProvider(adapterFactory));
-				selectionViewer.setUseHashlookup(true);
 
 				selectionViewer.setLabelProvider(new AdapterFactoryLabelProvider(adapterFactory));
 				selectionViewer.setInput(editingDomain.getResourceSet());
@@ -1342,7 +1344,6 @@ public class BehaviourEditor
 
 					// Set up the tree viewer.
 					//
-					contentOutlineViewer.setUseHashlookup(true);
 					contentOutlineViewer.setContentProvider(new AdapterFactoryContentProvider(adapterFactory));
 					contentOutlineViewer.setLabelProvider(new AdapterFactoryLabelProvider(adapterFactory));
 					contentOutlineViewer.setInput(editingDomain.getResourceSet());
@@ -1490,9 +1491,7 @@ public class BehaviourEditor
 					// Save the resources to the file system.
 					//
 					boolean first = true;
-					List<Resource> resources = editingDomain.getResourceSet().getResources();
-					for (int i = 0; i < resources.size(); ++i) {
-						Resource resource = resources.get(i);
+					for (Resource resource : editingDomain.getResourceSet().getResources()) {
 						if ((first || !resource.getContents().isEmpty() || isPersisted(resource)) && !editingDomain.isReadOnly(resource)) {
 							try {
 								long timeStamp = resource.getTimeStamp();
---
+++ xtendCodegenerator/bin/xtendcodegenerator/ToAnyLanguage.xtend
@@ -0,0 +1,24 @@
+package xtendcodegenerator
+import behaviour.Behaviour;
+import structural.Program;
+
+abstract class ToAnyLanguage {
+	protected Behaviour behav;
+	protected Program prog;
+	protected String endFile;
+	
+	def String runCodeGenerator(Behaviour behv,Program prog) {
+		this.behav=behv;
+		this.prog=prog;
+		genCode(behav,prog);
+		
+	}
+	def abstract String genCode(Behaviour behv,Program prog) ;
+	
+	def String getEndFile(){
+		this.endFile;
+		
+	}
+	
+	
+}
